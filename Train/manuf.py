b946b2bcc6e2d7355f3a17a732a19b7bd8cf9cb7
Alex Karpov <keyfour13@gmail.com>
2021-09-08 19:39:42 +0300

    Add output, prettify the code

--- Train/manuf.py
+++ Train/manuf.py
@@ -2,22 +2,32 @@ import argparse
 from html.parser import HTMLParser
 from transitions import Machine
 
+
 class ManufParser(object):
     states = ['ul_starttag', 'li_starttag', 'a_starttag', 'data', 'li_endtag']
 
     def __init__(self):
         self.state = None
-        self.machine = Machine(model=self, states=ManufParser.states, initial='ul_starttag')
-        self.machine.add_transition(trigger='search_li', source='ul_starttag', dest='li_starttag')
-        self.machine.add_transition(trigger='search_li', source='li_endtag', dest='li_starttag')
-        self.machine.add_transition(trigger='search_a', source='li_starttag', dest='a_starttag')
-        self.machine.add_transition(trigger='search_ul', source='a_starttag', dest='ul_starttag')
-        self.machine.add_transition(trigger='search_ul', source='li_starttag', dest='ul_starttag')
-        self.machine.add_transition(trigger='search_ul', source='li_endtag', dest='ul_starttag')
-        self.machine.add_transition(trigger='search_ul', source='data', dest='ul_starttag')
-        self.machine.add_transition(trigger='search_data', source='a_starttag', dest='data')
-        self.machine.add_transition(trigger='search_liend', source='data', dest='li_endtag')
-
+        self.machine = Machine(
+            model=self, states=ManufParser.states, initial='ul_starttag')
+        self.machine.add_transition(
+            trigger='search_li', source='ul_starttag', dest='li_starttag')
+        self.machine.add_transition(
+            trigger='search_li', source='li_endtag', dest='li_starttag')
+        self.machine.add_transition(
+            trigger='search_a', source='li_starttag', dest='a_starttag')
+        self.machine.add_transition(
+            trigger='search_ul', source='a_starttag', dest='ul_starttag')
+        self.machine.add_transition(
+            trigger='search_ul', source='li_starttag', dest='ul_starttag')
+        self.machine.add_transition(
+            trigger='search_ul', source='li_endtag', dest='ul_starttag')
+        self.machine.add_transition(
+            trigger='search_ul', source='data', dest='ul_starttag')
+        self.machine.add_transition(
+            trigger='search_data', source='a_starttag', dest='data')
+        self.machine.add_transition(
+            trigger='search_liend', source='data', dest='li_endtag')
 
 
 class ManufHTMLParser(HTMLParser):
@@ -39,7 +49,6 @@ class ManufHTMLParser(HTMLParser):
         elif self.machine.state == 'a_starttag' and tag == 'a':
             self.machine.search_data()
 
-
     def handle_endtag(self, tag):
         if self.machine.state == 'li_endtag' and tag == 'li':
             self.machine.search_li()
@@ -48,9 +57,9 @@ class ManufHTMLParser(HTMLParser):
 
     def handle_data(self, data):
         if self.machine.state == 'data':
-            manuf = data.strip()
-            if len(manuf) > 0:
-                self.manufacturers.append(manuf)
+            manuf = [name.strip() for name in data.split('/')]
+            self.manufacturers.extend(m for m in manuf if m != '')
+
 
 def read_html(path):
     parser = ManufHTMLParser()
@@ -58,9 +67,20 @@ def read_html(path):
         html = f.read()
         parser.feed(html)
     print(parser.manufacturers)
+    return parser.manufacturers
+
+
+def write_output(file, output):
+    with open(file, "w") as f:
+        f.write(",".join(name for name in output))
+
 
 if __name__ == "__main__":
     argparse = argparse.ArgumentParser()
-    argparse.add_argument("-f", "--file", required=True, help="Path to HTML file")
+    argparse.add_argument("-f", "--file", required=True,
+                          help="Path to HTML file")
+    argparse.add_argument("-o", "--output", help="Output file")
     args = argparse.parse_args()
-    read_html(args.file)
+    manufacturers = read_html(args.file)
+    if args.output:
+        write_output(args.output, manufacturers)
